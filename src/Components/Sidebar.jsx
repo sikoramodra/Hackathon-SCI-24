0666bb5b487957c780d666fd0d9a5a2dd35add01
sikoramodra <wojciechmodro@gmail.com>
2024-11-22 21:22:35 +0100

    fix faker error

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -16,7 +16,7 @@ export default function Sidebar() {
             alt="agent"
             onClick={handleAgentClick(val)}
           />
-          <div className="ml-1 hidden min-w-48 rounded bg-gray-300 p-2 shadow-md group-hover:block">
+          <div className="ml-1 hidden min-w-52 rounded bg-gray-300 p-2 shadow-md group-hover:block">
             <p className="mb-2 text-sm text-gray-600">
               <span className="font-semibold">Age: </span>
               {testAgent.age}
--- src/logic/agentGenerator.js
+++ src/logic/agentGenerator.js
@@ -1,58 +1,65 @@
-import globalVars from "./globalVariables";
-import { fakerPL } from "@faker-js/faker/.";
-import Agent from "./agent";
+import globalVars from './globalVariables';
+import Agent from './agent';
+import { faker } from '@faker-js/faker';
 
-const specialties = [
-    'medic',
-    'spy',
-    'killer',
-    'engineer',
-]
+const specialties = ['medic', 'spy', 'killer', 'engineer'];
 
 // quality okresla jakie szanse ma agent na bycie dobrym od 1/20
 const generateAgent = (quality) => {
-    const age = __getRandomIntInclusive(18, 70);
-    const sex = __generateSex()
-    const specialty = specialties[__getRandomIntInclusive(0, specialties.length - 1)]
-    const fullName = fakerPL.fullName({ sex: sex})
-    const [effectiveRangeStart, effectiveRangeEnd] = __generateEffectiveRange(quality)
+  const age = __getRandomIntInclusive(18, 70);
+  const sex = __generateSex();
+  const specialty =
+    specialties[__getRandomIntInclusive(0, specialties.length - 1)];
+  const fullName = faker.person.fullName({ sex: sex });
+  const [effectiveRangeStart, effectiveRangeEnd] =
+    __generateEffectiveRange(quality);
 
-    const agent = new Agent(sex, age, fullName, quality, specialty, effectiveRangeStart, effectiveRangeEnd)
-    return agent
+  const agent = new Agent(
+    sex,
+    age,
+    fullName,
+    quality,
+    specialty,
+    effectiveRangeStart,
+    effectiveRangeEnd,
+  );
+  return agent;
+};
+
+function __generateSex() {
+  let sexIndex = __getRandomIntInclusive(1, 2);
+
+  if (sexIndex === 1) {
+    return 'male';
+  } else {
+    return 'female';
+  }
 }
 
-function __generateSex(){
-    let sexIndex = __getRandomIntInclusive(1, 2);
+function __generateEffectiveRange(quality) {
+  const minRangeLength = quality * 5;
+  const maxRangeLength = minRangeLength + quality * 10;
+  const rangeLength = __getRandomIntInclusive(minRangeLength, maxRangeLength);
 
-    if(sexIndex === 1){
-        return 'male'
-    } else{
-        return 'female'
-    }
-}
+  let rangeStart =
+    globalVars['TIME_END'] -
+    __getRandomIntInclusive(globalVars['TIME_START'], globalVars['TIME_END']);
+  if (rangeStart < globalVars['TIME_START']) {
+    rangeStart = globalVars['TIME_START'];
+  }
 
-function __generateEffectiveRange(quality){
-    const minRangeLength = quality * 5
-    const maxRangeLength = minRangeLength + quality * 10
-    const rangeLength = __getRandomIntInclusive(minRangeLength, maxRangeLength)
+  const rangeEnd = rangeStart + rangeLength;
+  if (rangeEnd > globalVars['TIME_END']) {
+    rangeEnd = globalVars['TIME_END'];
+  }
 
-    let rangeStart = globalVars['TIME_END'] - __getRandomIntInclusive(globalVars['TIME_START'], globalVars['TIME_END'])
-    if(rangeStart < globalVars['TIME_START']){
-        rangeStart = globalVars['TIME_START']
-    }
-
-    const rangeEnd = rangeStart + rangeLength
-    if(rangeEnd > globalVars['TIME_END']){
-        rangeEnd = globalVars['TIME_END']
-    }
-
-    return [rangeStart, rangeEnd]
+  return [rangeStart, rangeEnd];
 }
 
 function __getRandomIntInclusive(min, max) {
-    const minCeiled = Math.ceil(min);
-    const maxFloored = Math.floor(max);
-    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
 }
 
 export default generateAgent;
