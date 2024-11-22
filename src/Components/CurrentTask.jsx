6350a71a9a4691ff2cd7855df9152deabece2b98
Grzegorz Kowaluk <grzkowaluk@gmail.com>
2024-11-23 00:40:07 +0100

    Adding visuals

--- src/Components/CurrentTask.jsx
+++ src/Components/CurrentTask.jsx
@@ -1,7 +1,13 @@
-export default function CurrentTask({currentTask}) {
-    return (
-      <div className="w-5/6 p-4 mx-auto border-2 border-gray-300 rounded-lg shadow-lg h-1/2">
-        <h2 className="mb-4 text-xl font-semibold text-center text-gray-700">{currentTask.description}</h2>
+import folderImage from '../assets/folder.png';
+export default function CurrentTask({ currentTask }) {
+  return (
+    <>
+      <img className="absolute z-0 w-1/2" src={folderImage} alt="" />
+      <div className="z-10 w-1/3 "></div>
+      <div className="z-10 w-2/5 p-6 mx-auto border border-black h-3/5">
+        <h2 className="mb-4 text-xl font-semibold text-center text-gray-700">
+          {currentTask.description}
+        </h2>
         <div className="space-y-2">
           <h3 className="text-sm text-gray-600">Requirements:</h3>
           <ul className="list-disc list-inside">
@@ -13,8 +19,8 @@ export default function CurrentTask({currentTask}) {
               );
             })}
           </ul>
-        </div>  
+        </div>
       </div>
-    );
-  }
-  
+    </>
+  );
+}
