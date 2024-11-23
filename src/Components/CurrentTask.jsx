95041770b29036b6948043e37b7556940d550851
zeeby <karolkleban@gmail.com>
2024-11-23 12:09:17 +0100

    readded fancy font

--- src/Components/CurrentTask.jsx
+++ src/Components/CurrentTask.jsx
@@ -47,7 +47,7 @@ export default function CurrentTask({
         )}
       </div>
       <div className="relative z-10 -mt-4 mr-auto h-3/5 w-2/5 p-6">
-        <h2 className="mb-4 overflow-hidden text-center text-xl font-semibold text-gray-700">
+        <h2 className="mb-4 overflow-hidden text-center text-xl font-semibold text-gray-700 shadows-into-light-regular">
           {currentTask.description}
         </h2>
         <div className="space-y-2">
